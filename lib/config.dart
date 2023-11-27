2816858fb982b59cd56780f88132672efd5e58f6
Alexander Adam <alexanderadamm21@gmail.com>
2023-11-28 00:01:54 +0700

    confg: add api endpoint configuration

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+String rootEndpoint = dotenv.get('API_ROOT_ENDPOINT');
+
+class Config {
+  static String menuEndpoint = "$rootEndpoint/menu";
+}
