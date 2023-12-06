4cf74c77f2a639afe87f0c6a6876dcdcdcbd45c3
FajarMaulid <mohammadfajarmaulid2004@mail.ugm.ac.id>
2023-12-06 19:30:31 +0700

    style: change main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,31 @@
+import 'package:cashier/screens/menu_screen.dart';
+import 'package:cashier/services/shared_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/welcome_screen.dart'; // Import the welcome screen
 
-void main() {
-  runApp(
-    ProviderScope(
-      // Wrap your app with ProviderScope to enable Riverpod
-      child: MyApp(),
-    ),
-  );
+Widget _defaultHome = const WelcomeScreen();
+void main() async {
+  await dotenv.load(fileName: '.env');
+  WidgetsFlutterBinding.ensureInitialized();
+
+  bool _isStarted = await SharedService.isStarted();
+  if (_isStarted) {
+    _defaultHome = const MenuScreen();
+  }
+  runApp(const ProviderScope(child: MyApp()));
 }
 
+final providerInitiaion = Provider<String>((_) => 'initialClass');
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CashHere App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const WelcomeScreen(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, routes: {
+      '/': (context) => _defaultHome,
+    });
   }
 }
